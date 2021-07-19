def865568814ce47bfaad30647fbbdf973775c5a
Ravi Sourav <ravisourav15@gmail.com>
2021-07-18 19:26:34 -0700

    Test

--- src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -22,6 +22,8 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    //This is 1st test
+    
     @Autowired
     private LocationStats locationStat;
     
